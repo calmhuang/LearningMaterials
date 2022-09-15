29ce56ba9182d2d0cbab3f42bbeb0b3ec93e9ac4
hqc <qc.huang@szsolar.cn>
2022-09-15 12:46:10 +0800

    Init:AOP资料（未完善）

---
+++ AOPLogTest/AopLogContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPLogTest
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class AopLogAttribute : ContextStaticAttribute, IContributeObjectSink
+    {
+        public IMessageSink GetObjectSink(MarshalByRefObject obj, IMessageSink nextSink)
+        {
+            return null;
+        }
+
+
+    }
+
+    [AopLog]
+    public class AopContext : ContextBoundObject
+    {
+
+
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class AopLogMethodAttribute : Attribute
+    {
+
+
+    }
+
+}
